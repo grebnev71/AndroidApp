7896c976baa20bb0655f2e67fd8fe4f1094844ff
grebnev71 <grebnev@yandex.ru>
2016-01-30 17:51:59 +0300

    second

--- AndroidApp/TestGit/app/src/main/java/com/example/testgit/testgit/LoginActivity.java
+++ AndroidApp/TestGit/app/src/main/java/com/example/testgit/testgit/LoginActivity.java
@@ -142,6 +142,9 @@ public class LoginActivity extends AppCompatActivity implements LoaderCallbacks<
             showProgress(true);
             mAuthTask = new UserLoginTask(email, password);
             mAuthTask.execute((Void) null);
+
+            System.out.println("email");
+
         }
     }
 
